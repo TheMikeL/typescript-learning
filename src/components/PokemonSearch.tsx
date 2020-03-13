89eaee69735cc047427a1d680140737afbfae665
Michael Lam <michaellam@Michaels-MacBook-Pro.local>
2020-03-13 13:45:10 -0400

    Converted to functional component

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import IUser from "../interfaces/User.interface";
 
 interface ISearchState {
@@ -13,82 +13,72 @@ interface IPokemon {
   imageUrl: string;
 }
 
-export class PokemonSearch extends Component<IUser, ISearchState> {
-  private pokemonRef: React.RefObject<HTMLInputElement>;
-  constructor(props: IUser) {
-    super(props);
-    this.state = {
-      error: false,
-      pokemon: null,
-    };
-    this.pokemonRef = React.createRef();
-  }
+const PokemonSearch = (props: IUser, state: ISearchState) => {
+  const { name: userName, numberOfPokemons } = props;
+  const [error, setError] = useState(false);
+  const [pokemon, setPokemon] = useState(null);
+  const pokemonRef: React.RefObject<HTMLInputElement> = React.createRef();
 
-  public render() {
-    const { name: userName, numberOfPokemons } = this.props;
-    const { error, pokemon } = this.state;
-    let resultMarkUp;
+  let resultMarkUp;
 
-    if (error) {
-      resultMarkUp = <p> Pokemon not found, please try again</p>;
-    } else if (pokemon) {
-      resultMarkUp = (
-        <div>
-          <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
-          <p>
-            {this.capitalize(pokemon.name)} has {pokemon.numberOfAbilities} abilities with{" "}
-            {pokemon.baseExperience} base experience point.
-          </p>
-        </div>
-      );
-    }
 
-    return (
-      <div>
-        <p>
-          User {userName}{" "}
-          {numberOfPokemons ? "has " + numberOfPokemons + " pokemons" : ""}
-        </p>
-        <input className="input-box" type="text" ref={this.pokemonRef} onKeyDown={this.onSearchKeyDown}/>
-        <button onClick={this.searchPokemon} className="my-button">
-          Search
-        </button>
-        {resultMarkUp}
-      </div>
-    );
-  }
-
-  private searchPokemon = (): void => {
-    const inputValue = (this.pokemonRef.current.value).toLowerCase();
+  const searchPokemon = (): void => {
+    const inputValue = (pokemonRef.current.value).toLowerCase();
     const baseURL = "https://pokeapi.co/api/v2/pokemon/";
     fetch(`${baseURL}${inputValue}`).then((res) => {
       if (res.status !== 200) {
-        this.setState({ error: true });
+        setError(true);
         return;
       }
       res.json().then((data) => {
-        this.setState({
-          error: false,
-          pokemon: {
-            baseExperience: data.base_experience,
-            imageUrl: data.sprites.front_default,
-            name: data.name,
-            numberOfAbilities: data.abilities.length,
-          },
+        setError(false);
+        setPokemon({
+          baseExperience: data.base_experience,
+          imageUrl: data.sprites.front_default,
+          name: data.name,
+          numberOfAbilities: data.abilities.length,
         });
       });
     });
   }
 
-  private onSearchKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const onSearchKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      this.searchPokemon();
+      searchPokemon();
     }
+  };
+
+  const capitalize = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  if (error) {
+    resultMarkUp = <p> Pokemon not found, please try again</p>;
+  } else if (pokemon) {
+    resultMarkUp = (
+      <div>
+        <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
+        <p>
+          {capitalize(pokemon.name)} has {pokemon.numberOfAbilities} abilities with{" "}
+          {pokemon.baseExperience} base experience point.
+        </p>
+      </div>
+    );
   }
 
-  private capitalize = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+  return (
+    <div>
+      <p>
+        User {userName}{" "}
+        {numberOfPokemons ? "has " + numberOfPokemons + " pokemons" : ""}
+      </p>
+      <input className="input-box" type="text" ref={this.pokemonRef} onKeyDown={onSearchKeyDown}/>
+      <button onClick={this.searchPokemon} className="my-button">
+        Search
+      </button>
+      {resultMarkUp}
+    </div>
+  );
 }
 
 export default PokemonSearch;
